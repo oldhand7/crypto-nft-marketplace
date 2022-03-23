952ae47b986a5c1e6d696e71d52fa74e064473fb
oldhanddev <oldhanddev@users.noreply.github.com>
2022-03-23 17:09:59 +0000

    deploy: 5970ef7043a2001e5a83cf9fe29b78a2b7d91de7

---
+++ precache-manifest.2ff500112d24ed2f1fc161a6abebb5a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "02035d9b9f0da13e33f5",
+    "url": "/cryptoboys-nft-marketplace/static/css/main.a5638c47.chunk.css"
+  },
+  {
+    "revision": "02035d9b9f0da13e33f5",
+    "url": "/cryptoboys-nft-marketplace/static/js/main.02035d9b.chunk.js"
+  },
+  {
+    "revision": "682ae8c1a9a6d39c1ff3",
+    "url": "/cryptoboys-nft-marketplace/static/js/1.682ae8c1.chunk.js"
+  },
+  {
+    "revision": "8bf4deafcc65d566b658",
+    "url": "/cryptoboys-nft-marketplace/static/js/runtime~main.8bf4deaf.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/cryptoboys-nft-marketplace/static/media/loading.26ef8bb4.gif"
+  },
+  {
+    "revision": "fbf33967fa244d21d61fb85f233fc331",
+    "url": "/cryptoboys-nft-marketplace/static/media/metamask.fbf33967.svg"
+  },
+  {
+    "revision": "1181078b0eb27e17bad3ed35038229ff",
+    "url": "/cryptoboys-nft-marketplace/index.html"
+  }
+];
